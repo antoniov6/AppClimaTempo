5a6ae493849842381b0e68890fa2bf62e076fc35
Antonio Rodrigues <antoniovkuhl@gmail.com>
2023-09-05 12:03:59 -0300

    final commit

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ const key = "d4afe77fa4726132a9e572c18912deee";
 
 function putDataOnScreen(data) {
   console.log(data);
-  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".city").innerHTML = "Tempo em " + data.name;
+  document.querySelector(".temp").innerHTML = Math.floor(data.main.temp) + "°C";
+  document.querySelector(
+    ".forecast-img"
+  ).src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+  document.querySelector(".forecast-txt").innerHTML =
+    data.weather[0].description;
+  document.querySelector(".humidity").innerHTML =
+    "Umidade: " + data.main.humidity + "%";
 }
 
 async function searchCity(city) {
   const data = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
   ).then((response) => response.json());
   putDataOnScreen(data);
 }
